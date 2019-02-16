25029178d78e66240418c15269fe5d23e0dbe2ac
Aung Pyae Phyo <aungpyaephyo@mm-digital-solutions.com>
2019-02-16 11:50:26 +0630

    translucent status bar & layout_collapseMode.

--- app/src/main/java/com/padcmyanmar/padc7/mmnews/activities/MainActivity.java
+++ app/src/main/java/com/padcmyanmar/padc7/mmnews/activities/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity implements NewsItemDelegate 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
--- app/src/main/java/com/padcmyanmar/padc7/mmnews/activities/NewsDetailsActivity.java
+++ app/src/main/java/com/padcmyanmar/padc7/mmnews/activities/NewsDetailsActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.padcmyanmar.padc7.mmnews.R;
 import com.padcmyanmar.padc7.mmnews.adapters.NewsDetailsImagesAdapter;
@@ -21,6 +22,9 @@ public class NewsDetailsActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_details);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         ViewPager vpNewsDetailsImages = findViewById(R.id.vp_news_details_images);
 
