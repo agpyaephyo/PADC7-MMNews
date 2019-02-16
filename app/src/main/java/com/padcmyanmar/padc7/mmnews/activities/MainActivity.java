c2feaf67f87ac0de8df4f0020e7b80d167beba90
Aung Pyae Phyo <aungpyaephyo@mm-digital-solutions.com>
2019-02-16 13:51:51 +0630

    DrawerLayout + NavigationView.

--- app/src/main/java/com/padcmyanmar/padc7/mmnews/activities/MainActivity.java
+++ app/src/main/java/com/padcmyanmar/padc7/mmnews/activities/MainActivity.java
@@ -2,8 +2,13 @@ package com.padcmyanmar.padc7.mmnews.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -11,6 +16,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.padcmyanmar.padc7.mmnews.R;
 import com.padcmyanmar.padc7.mmnews.adapters.NewsAdapter;
@@ -18,20 +24,71 @@ import com.padcmyanmar.padc7.mmnews.delegates.NewsItemDelegate;
 
 public class MainActivity extends AppCompatActivity implements NewsItemDelegate {
 
+    private BottomSheetBehavior<NestedScrollView> mBottomSheetBehavior;
+
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mToolbar = findViewById(R.id.toolbar);
+        mToolbar.setTitle(R.string.title_latest_news);
+
+        setSupportActionBar(mToolbar);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mNavigationView = findViewById(R.id.navigation_view);
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.menu_lastest_news:
+                        Toast.makeText(getApplicationContext(), getString(R.string.title_latest_news), Toast.LENGTH_SHORT).show();
+                        mToolbar.setTitle(R.string.title_latest_news);
+                        break;
+                    case R.id.menu_news_just_for_you:
+                        Toast.makeText(getApplicationContext(), getString(R.string.title_news_just_for_you), Toast.LENGTH_SHORT).show();
+                        mToolbar.setTitle(R.string.title_news_just_for_you);
+                        break;
+                    case R.id.menu_favorite_news:
+                        Toast.makeText(getApplicationContext(), getString(R.string.title_favorite_news), Toast.LENGTH_SHORT).show();
+                        mToolbar.setTitle(R.string.title_favorite_news);
+                        break;
+                }
+                for (int index = 0; index < mNavigationView.getMenu().size(); index++) {
+                    mNavigationView.getMenu().getItem(index).setChecked(false);
+                }
+                menuItem.setChecked(true);
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+                return false;
+            }
+        });
+
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDrawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                /*
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                        */
+
+                if (mBottomSheetBehavior.getState() == BottomSheetBehavior.STATE_COLLAPSED) {
+                    mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                } else {
+                    mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                }
             }
         });
 
@@ -41,6 +98,11 @@ public class MainActivity extends AppCompatActivity implements NewsItemDelegate 
 
         NewsAdapter newsAdapter = new NewsAdapter(this);
         rvNews.setAdapter(newsAdapter);
+
+        NestedScrollView nsvBottomSheet = findViewById(R.id.nsv_bottom_sheet);
+        mBottomSheetBehavior = BottomSheetBehavior.from(nsvBottomSheet);
+
+        mBottomSheetBehavior.setPeekHeight(0);
     }
 
     @Override
---
+++ app/src/main/java/com/padcmyanmar/padc7/mmnews/views/pods/LoginUserViewPod.java
@@ -0,0 +1,20 @@
+package com.padcmyanmar.padc7.mmnews.views.pods;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+public class LoginUserViewPod extends RelativeLayout {
+
+    public LoginUserViewPod(Context context) {
+        super(context);
+    }
+
+    public LoginUserViewPod(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public LoginUserViewPod(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
